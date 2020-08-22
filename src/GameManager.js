a535185385ed5d98aacbf34f48736bf26d251018
GreenRamSilgen <ps3iswow@gmail.com>
2020-08-21 18:17:22 -0700

    update css and img addition

--- src/GameManager.js
+++ src/GameManager.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Board} from './Board';
 import {PieceHolder} from './PieceHolder';
 import {Ship} from './Ship';
+import './GameManager.css';
+
 
 export class GameManager extends React.Component{
     constructor(props){
@@ -27,6 +29,7 @@ export class GameManager extends React.Component{
 
     selectPiece(event){
         let shipId = event.target.value;
+        console.log(shipId);
         if(shipId >5){ //player 2
             this.setState({
                 selectedPiece: this.p2Ships[shipId-6]
@@ -40,8 +43,8 @@ export class GameManager extends React.Component{
             // this.selectedPiece = this.p1Ships[shipId-1];
         }
     }
-    changeOrientation(event){
-        let shipId = event.target.value;
+    changeOrientation(shipId){
+        
         if(shipId >5){ //player 2
             this.p2Ships[shipId-6].orientationChange();
         }
@@ -52,11 +55,11 @@ export class GameManager extends React.Component{
 
     getAllShips(val){
         let arr = [];
-        arr.push(Ship({id:1+val,health:5,isVertical:false}));//Carrier
+        arr.push(Ship({id:1+val,health:5,isVertical:false}));//Destroyer
         arr.push(Ship({id:2+val,health:4,isVertical:false}));//Battleship
         arr.push(Ship({id:3+val,health:3,isVertical:false}));//Cruiser
-        arr.push(Ship({id:4+val,health:3,isVertical:false}));//Submarine
-        arr.push(Ship({id:5+val,health:2,isVertical:false}));//Destroyer
+        arr.push(Ship({id:4+val,health:3,isVertical:false}));//Carrier
+        arr.push(Ship({id:5+val,health:2,isVertical:false}));//Submarine
         return arr;
     }
 
@@ -80,15 +83,18 @@ export class GameManager extends React.Component{
     }
     render(){
         return(
-            <div>
+            <div className="game">
             {(this.state.winner) ? "PLAYER " + this.state.winner + " WINS!" : null}
-            {(this.state.playMode) ? null : <PieceHolder pieceSelected={this.selectPiece} pieces={this.p1Ships} changeOrientation={this.changeOrientation} />} 
-                
+            
+            <div className="p1Board">
+                {(this.state.playMode) ? null : <PieceHolder boardId={1} pieceSelected={this.selectPiece} pieces={this.p1Ships} changeOrientation={this.changeOrientation} />} 
                 <Board boardId={1} selectedShip={this.state.selectedPiece} isP1Turn={this.state.p1Turn} playMode={this.state.playMode} changeTurn={this.changeTurn} setWinner={this.updateWinner} gameOver={this.state.winner}/>
-                <br></br>
+            </div>
+            <div className="p2Board">
+                {(this.state.playMode) ? null : <PieceHolder boardId={2} pieceSelected={this.selectPiece} pieces={this.p2Ships} changeOrientation={this.changeOrientation} />}
                 <Board boardId={2} selectedShip={this.state.selectedPiece} isP1Turn={this.state.p1Turn} playMode={this.state.playMode} changeTurn={this.changeTurn} setWinner={this.updateWinner} gameOver={this.state.winner}/>
-            {(this.state.playMode) ? null : <PieceHolder pieceSelected={this.selectPiece} pieces={this.p2Ships} changeOrientation={this.changeOrientation} />}
-                <button onClick={this.startGame}>Start</button>
+            </div>
+            <button onClick={this.startGame}>Start</button>
             </div>
         );
     }
--- src/PieceHolder.js
+++ src/PieceHolder.js
@@ -1,19 +1,76 @@
 import React from 'react';
-
+import './PieceHolder.css';
 export class PieceHolder extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            shipOri : Array(10).fill(false,0),
+        }
+        this.getShipClass = this.getShipClass.bind(this);
+        this.getShipImage = this.getShipImage.bind(this);
+        this.changeOriState = this.changeOriState.bind(this);
     }
 
+    
+    getShipClass(id){
+        let temp = id;
+        if(id > 5) temp -= 5;
+
+        switch(temp){
+            case 1:
+                return "btnDestroyer"
+            case 2:
+                return "btnBattleship"
+            case 3:
+                return "btnCruiser";
+            case 4:
+                return "btnCarrier";
+            case 5: 
+            return "btnSubmarine";
+            default:
+                return null;
+        }
+    }
+
+        
+    getShipImage(id){
+        let temp = id;
+        if(id > 5) temp -= 5;
+
+        switch(temp){
+            case 1:
+                return (<img src='/assets/images/Destroyer.GIF' style={{visibility:"hidden"}} alt="Destroyer Ship"/>);
+            case 2:
+                return (<img src='/assets/images/Battleship.GIF' style={{visibility:"hidden"}} alt="Battleship"/>);
+            case 3:
+                return (<img src='/assets/images/Cruiser.GIF' style={{visibility:"hidden"}} alt="Cruiser Ship"/>);
+            case 4:
+                return (<img src='/assets/images/Carrier.GIF' style={{visibility:"hidden"}} alt="Aircraft Carrier Ship"/>);
+            case 5: 
+            return (<img src='/assets/images/Submarine.GIF' style={{visibility:"hidden"}} alt="Sumbmarine"/>);
+            default:
+                return null;
+        }
+    }
+
+    changeOriState(event){
+        let temp = [...this.state.shipOri];
+        temp[event.target.value] = !temp[event.target.value];
+        this.setState({
+            shipOri: temp,
+        })
+        this.props.changeOrientation(event.target.value);
+    }
     render(){
         return (
             <div>
+            {(this.props.boardId === 1) ? "PLAYER 1's SHIPS" : "PLAYER 2's SHIPS"}
             {
                 this.props.pieces.map((singleShip)=>{
                     return (
                         <div className="shipBtn" key={singleShip.id}>
-                        <button  value={singleShip.id} onClick={this.props.pieceSelected}>Ship {singleShip.id}</button>
-                        <button value={singleShip.id} onClick={this.props.changeOrientation}>Orientation</button>
+                        <button  className={"shipImgBtn " + this.getShipClass(singleShip.id)} value={singleShip.id} onClick={this.props.pieceSelected}>{this.getShipImage(singleShip.id)}</button>
+                        <button value={singleShip.id} onClick={this.changeOriState}>{(this.state.shipOri[singleShip.id]) ? "|" : "—"}</button>
                         </div>
                     );
                 })
